368437b66ff12bf3cab9d85ea5aab8d2d4667d48
Suryansh1987 <22054137@kiit.ac.in>
2025-04-03 10:53:37 +0530

    changedthekey

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_THe1ldzBVhI4@ep-square-haze-a5eh5wft-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require" // ✅ Fixed key
+    url: "sqldatabase"
   }
 });
